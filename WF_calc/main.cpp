8e72fde30ad9e7e63a455819014cbacf9267fc80
Жан Уразбеков <urazbekovb@mail.ru>
2018-01-25 22:18:56 +0100

    Initial Commit

---
+++ WF_calc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WF_calc
+//
+//  Created by Bakytzhan on 25.01.2018.
+//  Copyright © 2018 Bakytzhan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
